0aee8b96bbb9c8c84f75f9b3870c550784511003
Thiago Xavier <thiagoxvo@gmail.com>
2019-07-20 14:04:20 +0200

    1-quiz-game: read user input and compare results

--- 1-quiz-game/main.go
+++ 1-quiz-game/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
@@ -19,9 +20,11 @@ func main() {
 	if err != nil {
 		log.Fatalln("Could not open the csv file", err)
 	}
-
 	r := csv.NewReader(csvfile)
 
+	scanner := bufio.NewScanner(os.Stdin)
+	score := 0
+	questions := 0
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -30,6 +33,19 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("Question: %s\n", record[0])
+
+		fmt.Printf("Question: %s \n", record[0])
+		fmt.Print("Answer: ")
+		scanner.Scan()
+		answer := scanner.Text()
+
+		if len(answer) == 0 {
+			log.Fatalln("Invalid answer!")
+		}
+		if answer == record[1] {
+			score++
+		}
+		questions++
 	}
+	fmt.Printf("Your scored: %v out of %v", score, questions)
 }
