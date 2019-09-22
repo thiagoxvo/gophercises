2d2cb02f4bb7f5e9dfb9267d0b045129d722b610
Thiago Xavier <thiagoxvo@gmail.com>
2019-09-22 13:00:56 +0200

    3/cyoa - add the http handler

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/thiagoxvo/gophercises/cyoa"
 )
 
 func main() {
+	port := flag.String("port", "3000", "the port to start the CYOA server")
 	file := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
-	fmt.Printf("Using the story in %s", *file)
+	fmt.Printf("Using the story in %s\n", *file)
 
 	f, err := os.Open(*file)
 	if err != nil {
@@ -23,5 +26,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %s\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", *port), h))
 }
