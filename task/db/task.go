6ee9d7649cded3df302bc7bba6e6d0b29760023a
Thiago Xavier <thiagoxvo@gmail.com>
2019-09-29 20:05:20 +0200

    6/task - add database methods

--- task/db/task.go
+++ task/db/task.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -26,4 +27,58 @@ func Init(dbPath string) error {
 		_, err := tx.CreateBucketIfNotExists(taskBucket)
 		return err
 	})
+}
+
+// CreateTask saves a task in the database
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key := itob(id)
+		return b.Put(key, []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+// AllTasks return all stored tasks
+func AllTasks() ([]Task, error) {
+	var tasks []Task
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+// DeleteTask deletes the task from the database
+func DeleteTask(key int) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		return b.Delete(itob(key))
+	})
+}
+
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
 }
