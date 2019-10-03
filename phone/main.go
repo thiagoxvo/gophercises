bb08d681a91d61731714a8a6e6de13a04428c29e
Thiago Xavier <thiagoxvo@gmail.com>
2019-10-03 16:02:52 +0200

    7/phone - add postgres connection

--- phone/main.go
+++ phone/main.go
@@ -1,6 +1,61 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "docker"
+	dbname   = "gophercises_phone"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+	err = resetDB(db, dbname)
+	must(err)
+	err = createDB(db, dbname)
+	must(err)
+
+	db.Close()
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	must(db.Ping())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func normalize(phone string) string {
 	var buf bytes.Buffer
